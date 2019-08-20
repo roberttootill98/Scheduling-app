55d6c9e77b45b3b3b4502851d8d1b0df9290ea3e
Rob T <Robert.Tootill98@gmail.com>
2019-08-20 17:00:50 +0100

    basic server architecture almost done

--- server/server.js
+++ server/server.js
@@ -1,1 +1,23 @@
 // main server file
+'use strict'
+
+const express = require('express');
+const app = express();
+const path = require('path');
+//const db = require('./database.js');
+
+app.listen(8080);
+app.use('/', express.static(path.join(__dirname, '/client/'), {extensions: 'html'}));
+//app.use('/', express.static('client', {extensions: 'html'}));
+/*
+app.use('/', express.static('client', {extensions: 'html'}));
+app.get('/*', (req, res) => {
+  let top = path.resolve('.');
+  top = top.slice(0, -7);
+
+  const dir = `${top}/client/index.html`;
+  res.sendFile(path.join(dir));
+});
+*/
+
+// server functions -- http verbs
