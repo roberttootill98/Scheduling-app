bdccec45e253717611ad1642ff091e3263021435
Rob T <Robert.Tootill98@gmail.com>
2019-08-24 12:26:41 +0100

    sql start

---
+++ server/db/populate.sql
@@ -0,0 +1,8 @@
+/* users */
+insert into Users (Name) values ("Me");
+insert into Users (Name) values ("That guy");
+/* scheduler */
+insert into Days (Day, Month, Year, UserID) values (4, 7, 2019, 0);
+insert into Days (Day, Month, Year, UserID) values (27, 10, 2019, 1);
+/* activities */
+insert into Days (Day, Month, Year, UserID, Location, Description) values (4, 7, 2019, 0, "Somewhere", "Something");
--- server/server.js
+++ server/server.js
@@ -16,3 +16,33 @@ app.get('/*', (req, res) => {
 });
 
 // server functions -- http verbs
+
+/**
+* gets all days from a month
+*/
+async function getDays(req, res) {
+  const month = req.query.month;
+
+  days = generateDays(month);
+
+  // search db for days in month
+  res.json(days);
+}
+
+/**
+* generates json array of days
+*/
+function generateDays(month){
+  let days = []
+
+  for(let i in range(monthDays[monthDays[months[month]]])) {
+    // search db for day in month for user
+    // if not found {
+      // createDay
+
+    days.append(day)
+  }
+}
+
+const months = ["January", "Febuary", "March", "April", "May", "June", "July", "August", "September", "October", "November", "December"];
+const monthDays = [31, 28, 31, 30, 31, 30, 31, 31, 30, 31, 30, 31];
