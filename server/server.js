81ee3b8c0e5a5117dc8159b5c53d3b3fb588e5f7
Rob T <Robert.Tootill98@gmail.com>
2019-08-24 10:56:29 +0100

    server now starting properly

--- server/server.js
+++ server/server.js
@@ -6,18 +6,13 @@ const app = express();
 const path = require('path');
 //const db = require('./database.js');
 
+// server start
 app.listen(8080);
-app.use('/', express.static(path.join(__dirname, '/client/'), {extensions: 'html'}));
-//app.use('/', express.static('client', {extensions: 'html'}));
-/*
-app.use('/', express.static('client', {extensions: 'html'}));
+
+app.use('/', express.static('client'))
 app.get('/*', (req, res) => {
   let top = path.resolve('.');
-  top = top.slice(0, -7);
-
-  const dir = `${top}/client/index.html`;
   res.sendFile(path.join(dir));
 });
-*/
 
 // server functions -- http verbs
